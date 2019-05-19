7106e3d0e0ca5ee3acb3082fa965259deccbe745
Robert Malai <rmalai@mobileinsight.com>
2019-05-20 00:34:22 +0300

    Removed the pipeline.

--- deploy-pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation deploy \
-    --template pipeline.yml \
-    --stack-name ${PWD##*/}-pipeline \
-    --capabilities CAPABILITY_IAM \
-    --capabilities CAPABILITY_NAMED_IAM
